8257cdd9da48a963f4b80ce2ff632891f2ad5542
Thadeu Esteves Jr <tadeuu@gmail.com>
2019-03-04 14:17:15 -0300

    adapter to chrome extension

--- src/todo-view.js
+++ src/todo-view.js
@@ -1,58 +1,56 @@
 function TodoView(model) {
-  this.model = model
-  this.component = new Observable(this)
+  this.model = model;
+  this.component = new Observable(this);
 
-  this.init()
-  this.preventSubmitForm()
+  this.init();
+  this.preventSubmitForm();
 }
 
 TodoView.prototype = {
-  init: function() {    
-    log('TodoView initialized')
+  init: function() {
+    log("TodoView initialized");
 
-    this.$container = $('.main-container')
-    this.$addTodoButton = this.$container.find('.add-todo-button')
-    this.$todoTextBox = this.$container.find('.todo-text-box')
-    this.$todoContainer = this.$container.find('.todo-container')
-    this.$formContainer = this.$container.find('form')
+    this.$container = $(".main-container");
+    this.$addTodoButton = this.$container.find(".add-todo-button");
+    this.$todoTextBox = this.$container.find(".todo-text-box");
+    this.$todoContainer = this.$container.find(".todo-container");
+    this.$formContainer = this.$container.find("form");
 
-    this.$addTodoButton.click(() => this.addTodoButtonClicked())
-    this.refreshTodoList()
+    this.$addTodoButton.click(() => this.addTodoButtonClicked());
+    this.refreshTodoList();
   },
 
   preventSubmitForm: function() {
     this.$formContainer.submit(function(e) {
-      e.preventDefault()
-    })
+      e.preventDefault();
+    });
   },
 
   addTodoButtonClicked: function() {
     this.component.notify({
       todo: this.$todoTextBox.val()
-    })
+    });
 
-    this.$todoTextBox.val('')
-    this.refreshTodoList()
+    this.$todoTextBox.val("");
+    this.refreshTodoList();
   },
 
   refreshTodoList: function() {
-    let todos = this.model.getTodos()
-    let $todoContainer = this.$todoContainer
+    let todos = this.model.getTodos();
+    let $todoContainer = this.$todoContainer;
 
-    $todoContainer.html('')
-    
+    $todoContainer.html("");
+
     let todoCard = todo => `<div class="flex justify-center mb-4 w-full">
       <div class="max-w-md w-full lg:flex">
-        <div class="border border-grey-light bg-white rounded-b p-4 flex flex-col justify-between leading-normal w-full">
-          <div class="">
-            <div class="text-black font-bold text-xl">${todo.name}</div>
-          </div>
+        <div class="border border-grey-light bg-white p-2 flex flex-col justify-between leading-normal w-full">
+          <div class="text-black font-bold">${todo.name}</div>
         </div>
       </div>
-    </div>`
+    </div>`;
 
-    for(let todo in todos) {
-      $todoContainer.append(todoCard(todos[todo]))
+    for (let todo in todos) {
+      $todoContainer.append(todoCard(todos[todo]));
     }
   }
-}
+};
